6314293bb80aac803ea707adb7d3dbcf6ce8c778
dolppe <dbtjdals9342@naver.com>
2024-07-19 22:30:22 +0900

    Experience 세팅 #1

---
+++ Source/BattleActionGame/Character/BattlePawnData.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "BattlePawnData.generated.h"
+
+
+UCLASS()
+class UBattlePawnData : public UPrimaryDataAsset
+{
+	GENERATED_BODY()
+public:
+	UBattlePawnData(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Battle|Pawn")
+	TSubclassOf<APawn> PawnClass;
+
+
+};
+
--- Source/BattleActionGame/GameModes/BattleExperienceManagerComponent.h
+++ Source/BattleActionGame/GameModes/BattleExperienceManagerComponent.h
@@ -1,4 +1,20 @@
 #pragma once
 
+#include "CoreMinimal.h"
+#include "Components/GameStateComponent.h"
+#include "BattleExperienceDefinition.h"
 #include "BattleExperienceManagerComponent.generated.h"
 
+UCLASS()
+class UBattleExperienceManagerComponent : public UGameStateComponent
+{
+	GENERATED_BODY()
+public:
+	UBattleExperienceManagerComponent(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+
+private:
+	UPROPERTY()
+	TObjectPtr<const UBattleExperienceDefinition> CurrentExperience;
+		
+};
