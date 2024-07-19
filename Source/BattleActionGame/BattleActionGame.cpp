30cda1de2dcfa57de4c8d6b1fe5a9d30f8692c53
dolppe <dbtjdals9342@naver.com>
2024-07-19 17:50:51 +0900

    언리얼 프로젝트 생성

---
+++ Source/BattleActionGame/BattleActionGame.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BattleActionGame.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BattleActionGame, "BattleActionGame" );
